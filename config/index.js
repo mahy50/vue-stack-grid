ae1044b826add815684c0b78523b7decf76993b8
mahy50 <934037783@qq.com>
2018-01-18 17:35:56 +0800

    fix; fix webpack and package.json

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../example-dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-stack-grid/',
 
     /**
      * Source Maps
